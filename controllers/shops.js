9b16debab05c813221d6d5257f1dcebc98e9f758
Jana Pascual <jacpascual@gmail.com>
2022-03-07 11:06:29 -0500

    renders index page

--- controllers/shops.js
+++ controllers/shops.js
@@ -2,12 +2,16 @@ import { Shop } from "../models/shop.js"
 
 function index(req, res) {
   Shop.find({})
-  .then(tacos => {
+  .then(shops => {
     res.render("shops/index", {
-      shops: "Ice Cream Shops",
+      shops,
       title: "Scoop"
 
     })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect("/shops")
   })
 }
 
