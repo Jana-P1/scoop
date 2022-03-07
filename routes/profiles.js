a74568816e38f575997f86e882fb122348e7df48
Jana Pascual <jacpascual@gmail.com>
2022-03-07 16:53:10 -0500

    renders index view for user profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,6 +1,11 @@
 import { Router } from "express"
+import { isLoggedIn } from "../middleware/middleware.js"
+import * as profilesCtrl from "../controllers/profiles.js"
 
 const router = Router()
+
+// GET - localhost:3000/profiles
+router.get("/", isLoggedIn, profilesCtrl.index)
 
 export {
   router
